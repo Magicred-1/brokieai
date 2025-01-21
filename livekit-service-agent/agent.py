dcef0e2af8cd874050b3794ada6a59aa7cd0dd75
Shreyas Papinwar <spapinwar@gmail.com>
2025-01-21 22:46:15 +0530

    Fixing call for non-sip trunk agent, adding gpt-4o realtime agent

--- livekit-service-agent/agent.py
+++ livekit-service-agent/agent.py
@@ -1,215 +1,324 @@
 from __future__ import annotations
 
 import asyncio
-import logging
-from dotenv import load_dotenv
 import json
-import os
-from time import perf_counter
-from typing import Annotated
-from livekit import rtc, api
+import logging
+import uuid
+from dataclasses import asdict, dataclass
+from typing import Any, Dict, Literal
+
+from livekit import rtc
 from livekit.agents import (
     AutoSubscribe,
     JobContext,
-    JobProcess,
     WorkerOptions,
+    WorkerType,
     cli,
     llm,
 )
 from livekit.agents.multimodal import MultimodalAgent
-from livekit.agents.pipeline import VoicePipelineAgent
-from livekit.plugins import deepgram, openai, silero
+from livekit.plugins import openai
 
+from dotenv import load_dotenv
 
-# load environment variables, this is optional, only used for local development
-load_dotenv(dotenv_path=".env.local")
-logger = logging.getLogger("outbound-caller")
+load_dotenv()
+
+logger = logging.getLogger("my-worker")
 logger.setLevel(logging.INFO)
 
-outbound_trunk_id = os.getenv("SIP_OUTBOUND_TRUNK_ID")
-_default_instructions = (
-    "You are an assistant for calling"
-)
+@dataclass
+class SessionConfig:
+    openai_api_key: str
+    instructions: str
+    voice: openai.realtime.api_proto.Voice
+    temperature: float
+    max_response_output_tokens: str | int
+    modalities: list[openai.realtime.api_proto.Modality]
+    turn_detection: openai.realtime.ServerVadOptions
+
+    def __post_init__(self):
+        if self.modalities is None:
+            self.modalities = self._modalities_from_string("text_and_audio")
+
+    def to_dict(self):
+        return {k: v for k, v in asdict(self).items() if k != "openai_api_key"}
+
+    @staticmethod
+    def _modalities_from_string(modalities: str) -> list[str]:
+        modalities_map = {
+            "text_and_audio": ["text", "audio"],
+            "text_only": ["text"],
+        }
+        return modalities_map.get(modalities, ["text", "audio"])
+
+    def __eq__(self, other: SessionConfig) -> bool:
+        return self.to_dict() == other.to_dict()
+
+
+def parse_session_config(data: Dict[str, Any]) -> SessionConfig:
+    turn_detection = None
+
+    if data.get("turn_detection"):
+        turn_detection_json = json.loads(data.get("turn_detection"))
+        turn_detection = openai.realtime.ServerVadOptions(
+            threshold=turn_detection_json.get("threshold", 0.5),
+            prefix_padding_ms=turn_detection_json.get("prefix_padding_ms", 200),
+            silence_duration_ms=turn_detection_json.get("silence_duration_ms", 300),
+        )
+    else:
+        turn_detection = openai.realtime.DEFAULT_SERVER_VAD_OPTIONS
+
+    config = SessionConfig(
+        openai_api_key=data.get("openai_api_key", ""),
+        instructions=data.get("instructions", ""),
+        voice=data.get("voice", "alloy"),
+        temperature=float(data.get("temperature", 0.8)),
+        max_response_output_tokens=data.get("max_output_tokens")
+        if data.get("max_output_tokens") == "inf"
+        else int(data.get("max_output_tokens") or 2048),
+        modalities=SessionConfig._modalities_from_string(
+            data.get("modalities", "text_and_audio")
+        ),
+        turn_detection=turn_detection,
+    )
+    return config
 
 
 async def entrypoint(ctx: JobContext):
-    global _default_instructions, outbound_trunk_id
     logger.info(f"connecting to room {ctx.room.name}")
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
 
-    user_identity = "phone_user"
-    # the phone number to dial is provided in the job metadata
-    phone_number = ctx.job.metadata
-    logger.info(f"dialing {phone_number} to room {ctx.room.name}")
+    participant = await ctx.wait_for_participant()
 
-    # look up the user's phone number and appointment details
-    instructions = (
-        _default_instructions
-        + "The customer's name is Jayden. His appointment is next Tuesday at 3pm."
-    )
+    run_multimodal_agent(ctx, participant)
 
-    # `create_sip_participant` starts dialing the user
-    await ctx.api.sip.create_sip_participant(
-        api.CreateSIPParticipantRequest(
-            room_name=ctx.room.name,
-            sip_trunk_id=outbound_trunk_id,
-            sip_call_to=phone_number,
-            participant_identity=user_identity,
-        )
-    )
-
-    # a participant is created as soon as we start dialing
-    participant = await ctx.wait_for_participant(identity=user_identity)
-
-    # start the agent, either a VoicePipelineAgent or MultimodalAgent
-    # this can be started before the user picks up. The agent will only start
-    # speaking once the user answers the call.
-    # run_voice_pipeline_agent(ctx, participant, instructions)
-    run_multimodal_agent(ctx, participant, instructions)
-
-    # in addition, you can monitor the call status separately
-    start_time = perf_counter()
-    while perf_counter() - start_time < 30:
-        call_status = participant.attributes.get("sip.callStatus")
-        if call_status == "active":
-            logger.info("user has picked up")
-            return
-        elif call_status == "automation":
-            # if DTMF is used in the `sip_call_to` number, typically used to dial
-            # an extension or enter a PIN.
-            # during DTMF dialing, the participant will be in the "automation" state
-            pass
-        elif call_status == "hangup":
-            # user hung up, we'll exit the job
-            logger.info("user hung up, exiting job")
-            break
-        await asyncio.sleep(0.1)
-
-    logger.info("session timed out, exiting job")
-    ctx.shutdown()
+    logger.info("agent started")
 
 
-class CallActions(llm.FunctionContext):
-    """
-    Detect user intent and perform actions
-    """
+def run_multimodal_agent(ctx: JobContext, participant: rtc.Participant):
+    metadata = json.loads(participant.metadata)
+    config = parse_session_config(metadata)
 
-    def __init__(
-        self, *, api: api.LiveKitAPI, participant: rtc.RemoteParticipant, room: rtc.Room
-    ):
-        super().__init__()
+    logger.info(f"starting MultimodalAgent with config: {config.to_dict()}")
 
-        self.api = api
-        self.participant = participant
-        self.room = room
-
-    async def hangup(self):
-        try:
-            await self.api.room.remove_participant(
-                api.RoomParticipantIdentity(
-                    room=self.room.name,
-                    identity=self.participant.identity,
-                )
-            )
-        except Exception as e:
-            # it's possible that the user has already hung up, this error can be ignored
-            logger.info(f"received error while ending call: {e}")
-
-    @llm.ai_callable()
-    async def end_call(self):
-        """Called when the user wants to end the call"""
-        logger.info(f"ending the call for {self.participant.identity}")
-        await self.hangup()
-
-    @llm.ai_callable()
-    async def look_up_availability(
-        self,
-        date: Annotated[str, "The date of the appointment to check availability for"],
-    ):
-        """Called when the user asks about alternative appointment availability"""
-        logger.info(
-            f"looking up availability for {self.participant.identity} on {date}"
-        )
-        asyncio.sleep(3)
-        return json.dumps(
-            {
-                "available_times": ["1pm", "2pm", "3pm"],
-            }
-        )
-
-    @llm.ai_callable()
-    async def confirm_appointment(
-        self,
-        date: Annotated[str, "date of the appointment"],
-        time: Annotated[str, "time of the appointment"],
-    ):
-        """Called when the user confirms their appointment on a specific date. Use this tool only when they are certain about the date and time."""
-        logger.info(
-            f"confirming appointment for {self.participant.identity} on {date} at {time}"
-        )
-        return "reservation confirmed"
-
-    @llm.ai_callable()
-    async def detected_answering_machine(self):
-        """Called when the call reaches voicemail. Use this tool AFTER you hear the voicemail greeting"""
-        logger.info(f"detected answering machine for {self.participant.identity}")
-        await self.hangup()
-
-
-def run_voice_pipeline_agent(
-    ctx: JobContext, participant: rtc.RemoteParticipant, instructions: str
-):
-    logger.info("starting voice pipeline agent")
-
-    initial_ctx = llm.ChatContext().append(
-        role="system",
-        text=instructions,
-    )
-
-    agent = VoicePipelineAgent(
-        vad=ctx.proc.userdata["vad"],
-        stt=deepgram.STT(model="nova-2-phonecall"),
-        llm=openai.LLM(),
-        tts=openai.TTS(),
-        chat_ctx=initial_ctx,
-        fnc_ctx=CallActions(api=ctx.api, participant=participant, room=ctx.room),
-    )
-
-    agent.start(ctx.room, participant)
-
-
-def run_multimodal_agent(
-    ctx: JobContext, participant: rtc.RemoteParticipant, instructions: str
-):
-    logger.info("starting multimodal agent")
+    if not config.openai_api_key:
+        raise Exception("OpenAI API Key is required")
 
     model = openai.realtime.RealtimeModel(
-        instructions=instructions,
-        modalities=["audio", "text"],
+        api_key=config.openai_api_key,
+        instructions=config.instructions,
+        voice=config.voice,
+        temperature=config.temperature,
+        max_response_output_tokens=config.max_response_output_tokens,
+        modalities=config.modalities,
+        turn_detection=config.turn_detection,
     )
-    agent = MultimodalAgent(
-        model=model,
-        fnc_ctx=CallActions(api=ctx.api, participant=participant, room=ctx.room),
-    )
-    agent.start(ctx.room, participant)
+    assistant = MultimodalAgent(model=model)
+    assistant.start(ctx.room)
+    session = model.sessions[0]
 
+    if config.modalities == ["text", "audio"]:
+        session.conversation.item.create(
+            llm.ChatMessage(
+                role="user",
+                content="Please begin the interaction with the user in a manner consistent with your instructions.",
+            )
+        )
+        session.response.create()
 
-def prewarm(proc: JobProcess):
-    proc.userdata["vad"] = silero.VAD.load()
+    @ctx.room.local_participant.register_rpc_method("pg.updateConfig")
+    async def update_config(
+        data: rtc.rpc.RpcInvocationData,
+    ):
+        if data.caller_identity != participant.identity:
+            return
+
+        new_config = parse_session_config(json.loads(data.payload))
+        if config != new_config:
+            logger.info(
+                f"config changed: {new_config.to_dict()}, participant: {participant.identity}"
+            )
+            session = model.sessions[0]
+            session.session_update(
+                instructions=new_config.instructions,
+                voice=new_config.voice,
+                temperature=new_config.temperature,
+                max_response_output_tokens=new_config.max_response_output_tokens,
+                turn_detection=new_config.turn_detection,
+                modalities=new_config.modalities,
+            )
+            return json.dumps({"changed": True})
+        else:
+            return json.dumps({"changed": False})
+
+    @session.on("response_done")
+    def on_response_done(response: openai.realtime.RealtimeResponse):
+        variant: Literal["warning", "destructive"]
+        description: str | None = None
+        title: str
+        if response.status == "incomplete":
+            if response.status_details and response.status_details["reason"]:
+                reason = response.status_details["reason"]
+                if reason == "max_output_tokens":
+                    variant = "warning"
+                    title = "Max output tokens reached"
+                    description = "Response may be incomplete"
+                elif reason == "content_filter":
+                    variant = "warning"
+                    title = "Content filter applied"
+                    description = "Response may be incomplete"
+                else:
+                    variant = "warning"
+                    title = "Response incomplete"
+            else:
+                variant = "warning"
+                title = "Response incomplete"
+        elif response.status == "failed":
+            if response.status_details and response.status_details["error"]:
+                error_code = response.status_details["error"]["code"]
+                if error_code == "server_error":
+                    variant = "destructive"
+                    title = "Server error"
+                elif error_code == "rate_limit_exceeded":
+                    variant = "destructive"
+                    title = "Rate limit exceeded"
+                else:
+                    variant = "destructive"
+                    title = "Response failed"
+            else:
+                variant = "destructive"
+                title = "Response failed"
+        else:
+            return
+
+        asyncio.create_task(show_toast(title, description, variant))
+
+    async def send_transcription(
+        ctx: JobContext,
+        participant: rtc.Participant,
+        track_sid: str,
+        segment_id: str,
+        text: str,
+        is_final: bool = True,
+    ):
+        transcription = rtc.Transcription(
+            participant_identity=participant.identity,
+            track_sid=track_sid,
+            segments=[
+                rtc.TranscriptionSegment(
+                    id=segment_id,
+                    text=text,
+                    start_time=0,
+                    end_time=0,
+                    language="en",
+                    final=is_final,
+                )
+            ],
+        )
+        await ctx.room.local_participant.publish_transcription(transcription)
+
+    async def show_toast(
+        title: str,
+        description: str | None,
+        variant: Literal["default", "success", "warning", "destructive"],
+    ):
+        await ctx.room.local_participant.perform_rpc(
+            destination_identity=participant.identity,
+            method="pg.toast",
+            payload=json.dumps(
+                {"title": title, "description": description, "variant": variant}
+            ),
+        )
+
+    last_transcript_id = None
+
+    # send three dots when the user starts talking. will be cleared later when a real transcription is sent.
+    @session.on("input_speech_started")
+    def on_input_speech_started():
+        nonlocal last_transcript_id
+        remote_participant = next(iter(ctx.room.remote_participants.values()), None)
+        if not remote_participant:
+            return
+
+        track_sid = next(
+            (
+                track.sid
+                for track in remote_participant.track_publications.values()
+                if track.source == rtc.TrackSource.SOURCE_MICROPHONE
+            ),
+            None,
+        )
+        if last_transcript_id:
+            asyncio.create_task(
+                send_transcription(
+                    ctx, remote_participant, track_sid, last_transcript_id, ""
+                )
+            )
+
+        new_id = str(uuid.uuid4())
+        last_transcript_id = new_id
+        asyncio.create_task(
+            send_transcription(
+                ctx, remote_participant, track_sid, new_id, "…", is_final=False
+            )
+        )
+
+    @session.on("input_speech_transcription_completed")
+    def on_input_speech_transcription_completed(
+        event: openai.realtime.InputTranscriptionCompleted,
+    ):
+        nonlocal last_transcript_id
+        if last_transcript_id:
+            remote_participant = next(iter(ctx.room.remote_participants.values()), None)
+            if not remote_participant:
+                return
+
+            track_sid = next(
+                (
+                    track.sid
+                    for track in remote_participant.track_publications.values()
+                    if track.source == rtc.TrackSource.SOURCE_MICROPHONE
+                ),
+                None,
+            )
+            asyncio.create_task(
+                send_transcription(
+                    ctx, remote_participant, track_sid, last_transcript_id, ""
+                )
+            )
+            last_transcript_id = None
+
+    @session.on("input_speech_transcription_failed")
+    def on_input_speech_transcription_failed(
+        event: openai.realtime.InputTranscriptionFailed,
+    ):
+        nonlocal last_transcript_id
+        if last_transcript_id:
+            remote_participant = next(iter(ctx.room.remote_participants.values()), None)
+            if not remote_participant:
+                return
+
+            track_sid = next(
+                (
+                    track.sid
+                    for track in remote_participant.track_publications.values()
+                    if track.source == rtc.TrackSource.SOURCE_MICROPHONE
+                ),
+                None,
+            )
+
+            error_message = "⚠️ Transcription failed"
+            asyncio.create_task(
+                send_transcription(
+                    ctx,
+                    remote_participant,
+                    track_sid,
+                    last_transcript_id,
+                    error_message,
+                )
+            )
+            last_transcript_id = None
 
 
 if __name__ == "__main__":
-    if not outbound_trunk_id or not outbound_trunk_id.startswith("ST_"):
-        raise ValueError(
-            "SIP_OUTBOUND_TRUNK_ID is not set. Please follow the guide at https://docs.livekit.io/agents/quickstarts/outbound-calls/ to set it up."
-        )
-    cli.run_app(
-        WorkerOptions(
-            entrypoint_fnc=entrypoint,
-            # giving this agent a name will allow us to dispatch it via API
-            # automatic dispatch is disabled when `agent_name` is set
-            agent_name="outbound-caller",
-            # prewarm by loading the VAD model, needed only for VoicePipelineAgent
-            prewarm_fnc=prewarm,
-        )
-    )
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, worker_type=WorkerType.ROOM))
