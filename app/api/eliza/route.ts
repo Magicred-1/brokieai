1ef521e1f3e63964cb7423d92ee539fc6411d544
Lucas Pereira <lucas.pereira@intercellar.io>
2025-01-23 22:22:54 +0100

    feat(wip): add jwt verification

--- app/api/eliza/route.ts
+++ app/api/eliza/route.ts
@@ -1,17 +1,24 @@
-import { createClient } from '@/utils/supabase';
-import { Keypair } from '@solana/web3.js';
-import { NextResponse } from 'next/server';
-import { v4 as uuidv4 } from 'uuid';
+import { createClient } from "@/utils/supabase";
+import { Keypair } from "@solana/web3.js";
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JwksClient } from "jwks-rsa";
 
-function generate_characters(name: string, description: string, publicKey: string, privateKey: string) {
+function generate_characters(
+  name: string,
+  description: string,
+  publicKey: string,
+  privateKey: string
+) {
   return {
     id: uuidv4(),
     name: name,
-    plugins: ['@elizaos/plugin-solana', '@elizaos/plugin-web-search'],
-    clients: ['direct'],
-    modelProvider: 'openai',
+    plugins: ["@elizaos/plugin-solana", "@elizaos/plugin-web-search"],
+    clients: ["direct"],
+    modelProvider: "openai",
     settings: {
-      voice: { model: 'en_US-hfc_female-medium' },
+      voice: { model: "en_US-hfc_female-medium" },
       secrets: {
         WALLET_PRIVATE_KEY: privateKey,
         WALLET_PUBLIC_KEY: publicKey,
@@ -19,11 +26,11 @@ function generate_characters(name: string, description: string, publicKey: strin
     },
     bio: [description],
     lore: [
-      'Secret Service allocations used for election interference.',
-      'Promotes WorldLibertyFi for crypto leadership.',
+      "Secret Service allocations used for election interference.",
+      "Promotes WorldLibertyFi for crypto leadership.",
     ],
     knowledge: [
-      'Understands border issues, Secret Service dynamics, and financial impacts on families.',
+      "Understands border issues, Secret Service dynamics, and financial impacts on families.",
     ],
     topics: [], // Ensure topics are included if needed
     adjectives: [], // Ensure adjectives are included if needed
@@ -31,14 +38,14 @@ function generate_characters(name: string, description: string, publicKey: strin
     messageExamples: [
       [
         {
-          user: '{{user1}}',
-          content: { text: 'What about the border crisis?' },
+          user: "{{user1}}",
+          content: { text: "What about the border crisis?" },
         },
       ],
     ],
     postExamples: [
-      'End inflation and make America affordable again.',
-      'America needs law and order, not crime creation.',
+      "End inflation and make America affordable again.",
+      "America needs law and order, not crime creation.",
     ],
   };
 }
@@ -51,40 +58,98 @@ function generateSolanaWallet() {
   return { publicKey, privateKey };
 }
 
+const DYNAMIC_ID = process.env.DYNAMIC_PROJECT_ID;
+
+const jwksUrl = `https://app.dynamic.xyz/api/v0/sdk/${DYNAMIC_ID}/.well-known/jwks`;
+
 export const POST = async (req: Request) => {
   try {
     // Expecting the agent's name in the request body
+    const encodedJwt = req.headers
+      .get("Authorization")
+      ?.split(" ")[1]
+      .replace(/"/g, "");
+
+    const client = new JwksClient({
+      jwksUri: jwksUrl,
+      rateLimit: true,
+      cache: true,
+      cacheMaxEntries: 5, // Maximum number of cached keys
+      cacheMaxAge: 600000, // Cache duration in milliseconds (10 minutes in this case))}
+    });
+
+    const signingKey = await client.getSigningKey();
+    const pubKey = signingKey.getPublicKey();
+
+    if (!encodedJwt) {
+      return NextResponse.json(
+        { error: "Authorization token is missing" },
+        { status: 401 }
+      );
+    }
+
+    try {
+      jwt.verify(encodedJwt, pubKey, {
+        ignoreExpiration: false,
+      }) as JwtPayload;
+    } catch (error) {
+      return NextResponse.json({ error: error }, { status: 401 });
+    }
+
+    const decodedToken = jwt.decode(encodedJwt) as JwtPayload;
+    // TODO : create type for decodedToken
+    const userAddress = decodedToken.verified_credentials.address; // extract the user's wallet address from the JWT
+
+    if (decodedToken.scopes.includes("requiresAdditionalAuth")) {
+      // Either reject or handle the scopes appropriately.
+      // `requiresAdditionalAuth` is the scope used to indicate that JWT requires additional verification such as MFA.
+      return NextResponse.json({ error: "Non Authorized" }, { status: 401 });
+    }
+
     const requestData = await req.json();
+    const { name, description } = requestData;
 
-    const { name, description, walletAddress } = requestData;
-
-    console.log('Request Data:', requestData); // Log incoming data to debug
+    console.log("Request Data:", requestData); // Log incoming data to debug
 
     // Validate name and walletAddress input
     if (!name) {
-      return NextResponse.json({ error: 'Name is required in the request body.' }, { status: 400 });
+      return NextResponse.json(
+        { error: "Name is required in the request body." },
+        { status: 400 }
+      );
     }
 
     if (!description) {
-      return NextResponse.json({ error: 'Description is required in the request body.' }, { status: 400 });
+      return NextResponse.json(
+        { error: "Description is required in the request body." },
+        { status: 400 }
+      );
     }
 
-    if (!walletAddress) {
-      return NextResponse.json({ error: 'Wallet Address is required in the request body.' }, { status: 400 });
+    if (!userAddress) {
+      return NextResponse.json(
+        { error: "Wallet Address is required in the request body." },
+        { status: 400 }
+      );
     }
 
     const { publicKey, privateKey } = generateSolanaWallet();
 
     // Generate character data
-    const character = generate_characters(name, description, publicKey, privateKey);
+    const character = generate_characters(
+      name,
+      description,
+      publicKey,
+      privateKey
+    );
 
-    console.log('Generated Character:', character); // Log the character data to ensure it includes all fields
+    console.log("Generated Character:", character); // Log the character data to ensure it includes all fields
 
     const supabase = await createClient();
 
     // Store agent in Supabase with 'active' flag and 'configuration' JSON
     const { data: supabaseData, error } = await supabase
-      .from('configuration')
+      .from("configuration")
       .insert([
         {
           id: character.id,
@@ -104,7 +169,7 @@ export const POST = async (req: Request) => {
             messageExamples: character.messageExamples, // Ensure message examples are included
             postExamples: character.postExamples, // Ensure post examples are included
           },
-          owner: walletAddress,
+          owner: userAddress,
         },
       ]);
 
@@ -112,12 +177,15 @@ export const POST = async (req: Request) => {
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
 
-    return NextResponse.json({ message: "Agent created successfully!", data: supabaseData })
+    return NextResponse.json({
+      message: "Agent created successfully!",
+      data: supabaseData,
+    });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
   }
 };
 
 export const GET = async () => {
-  return NextResponse.json({ message: 'Hello World!' });
+  return NextResponse.json({ message: "Hello World!" });
 };
