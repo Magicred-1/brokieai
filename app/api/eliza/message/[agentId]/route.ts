c1719b2287414fa4286f89acb65f84fae1514ad6
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2025-01-27 22:16:57 +0100

    Update route.ts

--- app/api/eliza/message/[agentId]/route.ts
+++ app/api/eliza/message/[agentId]/route.ts
@@ -3,10 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
 import fs from 'fs/promises';
 
-export const POST = async (
-    req: NextRequest,
-    context: { params: { agentId: string } }
-) => {
+type Props = {
+    params: Promise<{
+      agentId: string;
+    }>;
+  };
+export const POST = async (req: Request, { params }: Props) =>{
     const { agentId } = context.params;
     const elizaUrl = process.env.ELIZA_API_URL;
 
