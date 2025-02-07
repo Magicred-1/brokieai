abcf122b2ffbec0a371fe1379ae080b215e5e76f
Djason Gadiou <djason.gadiou.sio@gmail.com>
2025-02-07 17:19:58 +0100

    Adding the Partners Logos sliders

--- components/ui/logo-slider.tsx
+++ components/ui/logo-slider.tsx
@@ -7,17 +7,14 @@ export function LogoSection() {
     <section className="w-full bg-[#131C2B] py-16">
       <div className="mx-auto max-w-6xl px-6 md:px-12 lg:px-16">
         <h2 className="text-center text-2xl font-bold md:text-3xl text-white">
-          Partners
+          Trusted by Innovative Companies
         </h2>
-        <p className="mt-2 text-center md:text-lg text-gray-300">
-            We are proud to be working with the following companies.
-        </p>
         <div
           className="relative mt-10 flex overflow-hidden p-4"
-          style={{
-            maskImage:
-              "linear-gradient(to left, transparent 0%, grey 20%, grey 80%, transparent 95%)",
-          }}
+          // style={{
+          //   maskImage:
+          //     "linear-gradient(to left, transparent 0%, grey 20%, grey 80%, transparent 95%)",
+          // }}
         >
           <motion.div
             className="flex items-center gap-8"
@@ -39,7 +36,7 @@ export function LogoSection() {
                   src={logo.url || "/placeholder.svg"}
                   width={150}
                   height={48}
-                  className="h-12 w-auto object-contain filter invert brightness-200"
+                  className="h-12 w-auto object-contain"
                   alt={logo.name}
                 />
               </div>
@@ -54,7 +51,7 @@ export function LogoSection() {
 const LOGOS = [
   {
     name: "Dynamic",
-    url: "https://cdn.prod.website-files.com/626692727bba3f384e008e8a/632d74b82fd2862796d5f6a0_logo-dark.svg",
+    url: "dynamic_brandkit/logo/svg/[Wordmark-secondary].svg",
   },
   {
     name: "ElizaOS",
@@ -62,7 +59,7 @@ const LOGOS = [
   },
   {
     name: "Supabase",
-    url: "supabase_brand_assets/supabase-logo-wordmark--light.svg",
+    url: "supabase_brand_assets/supabase-logo-wordmark--dark.svg",
   },
   {
     name: "Vercel",
